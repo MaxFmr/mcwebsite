8699909b8de05b8fc377f49ad0c86b4c873e56b8
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2022-11-15 16:56:53 +0100

    firstcommit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,37 @@ import React from 'react';
 import Header from '../components/Layout/Header/Header';
 import Footer from '../components/Layout/Footer/Footer';
 import HomeMain from '../components/Home/HomeMain';
+import Head from 'next/head';
 
 class Index extends React.Component {
+  static getInitialProps({ store }) {}
 
-    static getInitialProps({store}) {}
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Head>
+          <title>MC Production</title>
 
-    render() {
-        return (
-            <React.Fragment>
-            <Header />
-            <HomeMain />
-            <Footer />
-            </React.Fragment>
-        );
-    }
+          <link rel='shortcut icon' href='/assets/img/logo/favicon.png' />
+          <meta
+            name='google-site-verification'
+            content='YvTIft4XGDXwZTAbiex5wwvb7V5t3fA9V3bL5VIOF10'
+          />
+          <meta
+            name='description'
+            content='MC Production Agence digitale, de développement de sites et applications web, production video, photos en Hauts-de-France basée à Cambrai et Valenciennes. Notre expertise, vos besoins, vos solutions. Mettons en valeur vos métiers.'
+          />
+        </Head>
+        <Header />
+        <HomeMain />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
-
 export default Index;
-
