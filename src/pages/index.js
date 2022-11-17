ec30f67f2e3a72d6780b7bbd7af350ae68c59f69
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2022-11-17 09:32:08 +0100

    redac

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ class Index extends React.Component {
           />
           <meta
             name='description'
-            content='MC Production Agence digitale, de développement de sites et applications web, video, production audiovisuelle, e-learning, sound design, mixage audio, musique à la demande, photos en Hauts-de-France basée à Cambrai et Valenciennes. Notre expertise, vos besoins, vos solutions. Mettons en valeur votre métier.'
+            content='MC Production digitale, agence digitale-video, de développement de sites et applications web, production audiovisuelle, e-learning, sound design, mixage audio, musique à la demande, photos en Hauts-de-France basée à Cambrai et Valenciennes. Notre expertise, vos besoins, vos solutions. Mettons en valeur votre métier.'
           />
         </Head>
         <Header />
